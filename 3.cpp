cc5dde92947264659815ca86e240969947dbbd4d
z2nn <20221187@stu.neu.edu.cn>
2023-11-21 01:21:58 +0800

    change_snake_color

--- 3.cpp
+++ 3.cpp
@@ -82,7 +82,7 @@ bool Snake::checkCollision() {
     // 检查是否碰撞，1/0
     Point head = body.front();
     return count(body.begin() + 1, body.end(), head) > 0;//vector中count()返回这个值出现的次数,从第二个部位开始数
-
+    
 }
 
 bool Snake::checkBoundary() {
@@ -100,7 +100,7 @@ void Snake::draw(Mat& gamemap) {
     }
     // 蛇身
         for ( Point segment : body) { //基于范围的for循环 (c++11),第一部分是范围内用于迭代的变量，第二部分则表示被迭代的范围。
-            rectangle(gamemap, Point(segment.x * cellSize, segment.y * cellSize), Point((segment.x + 1) * cellSize, (segment.y + 1) * cellSize), Scalar(0, 0, 255), FILLED);
+            rectangle(gamemap, Point(segment.x * cellSize, segment.y * cellSize), Point((segment.x + 1) * cellSize, (segment.y + 1) * cellSize), Scalar(255, 0, 0), FILLED);
     }
     //食物
         rectangle(gamemap, Point(food.x * cellSize, food.y * cellSize), Point((food.x + 1) * cellSize, (food.y + 1) * cellSize), Scalar(0, 255, 0), FILLED);
